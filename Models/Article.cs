3db1d40e44640f8ef3672348b9b6213ca6095cce
minhduyet129 <minhduyet129@gmail.com>
2021-11-06 17:01:35 +0700

    Tao trang CRUD paging

--- Models/Article.cs
+++ Models/Article.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -11,14 +12,19 @@ namespace entityframework.Models
   {
     [Key]
     public int Id {get;set;}
-    [StringLength(255)]
-    [Required]
+    [StringLength(255,MinimumLength =5,ErrorMessage ="{0} phải dài từ {2} đến {1}")]
+    [Required(ErrorMessage ="{0} phai nhập")]
     [Column(TypeName ="nvarchar")]
+    [DisplayName("Tiêu đề")]
     public string Title{get;set;}
     [DataType(DataType.Date)]
-    [Required]
+    [Required(ErrorMessage ="{0} phải nhập")]
+    [DisplayName("Ngày tạo")]
+
     public DateTime Created{get;set;}
     [Column(TypeName ="ntext")]
+    [DisplayName("Nội dung")]
+
     public string Content {get;set;}
   }
 }
---
+++ Pages/Blog/Edit.cshtml.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using entityframework.Models;
+
+namespace _09EntityFramework.Pages_Blog
+{
+    public class EditModel : PageModel
+    {
+        private readonly entityframework.Models.MyBlogContext _context;
+
+        public EditModel(entityframework.Models.MyBlogContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Article Article { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return Content("Không thấy bài viết");
+            }
+
+            Article = await _context.articles.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Article == null)
+            {
+                return Content("Không thấy bài viết");
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Article).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticleExists(Article.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return _context.articles.Any(e => e.Id == id);
+        }
+    }
+}
